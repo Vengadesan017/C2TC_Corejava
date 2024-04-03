f2d5c88dae379bb1c5723e45bbc196e57c2f3b47
manikandan <manikandanperumal00@gmail.com>
2024-04-03 12:32:02 +0530

    aSJB

--- C37_Corejava/src/com/tns/dayone/SecondProgram.java
+++ C37_Corejava/src/com/tns/dayone/SecondProgram.java
@@ -2,8 +2,23 @@ package com.tns.dayone;
 
 public class SecondProgram {
 	
-	public static void main(String[] args) {
-		System.out.println("second");
+	private void address() {
+		// TODO Auto-generated method stub
+		System.out.println("Chennai");
 	}
 
-}
+	private void name() {
+		// TODO Auto-generated method stub
+       System.out.println("manikandan");
+	}
+
+	public static void main(String[] args) {
+
+	SecondProgram a=new SecondProgram();
+
+	a.address();
+	a.name();
+	}
+	}
+	
+	
