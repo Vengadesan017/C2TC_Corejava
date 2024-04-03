52c7fcc90a43dd33ca3494678f47b54db92a15ed
manikandan <manikandanperumal00@gmail.com>
2024-04-03 12:54:52 +0530

    first

--- C37_Corejava/src/com/tns/dayone/FirstProgram.java
+++ C37_Corejava/src/com/tns/dayone/FirstProgram.java
@@ -1,9 +1,7 @@
 package com.tns.dayone;
 
 public class FirstProgram {
-	
 	public static void main(String[] args) {
-		System.out.println("firstprogram");
-	}
-	
+		System.out.println("Hello World");
+		}
 }
