a0e3df5b4a6ef372a146ccceb9f9683cf4ae3950
User <User@MSI>
2024-04-17 08:43:25 +0530

    final

---
+++ C37_Corejava/src/com/tns/dayfour/Executors.java
@@ -0,0 +1,14 @@
+package com.tns.dayfour;
+
+import com.tns.dayfour.firstpackage.Base;
+	public class Executors {
+	public static void main(String[] args) {
+	
+	Base b1 = new Base();
+	b1.methodPublic();
+	b1.varPublic = 41;
+	b1.methodPublic();
+	}
+
+
+}
---
+++ C37_Corejava/src/com/tns/dayfour/MyClass.java
@@ -0,0 +1,5 @@
+package com.tns.dayfour;
+
+public class MyClass {
+
+}
