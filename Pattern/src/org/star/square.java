0e20f66a79b07a9aa2ee6c124878ca025504f88f
User <User@192.168.1.40>
2024-04-17 20:42:43 +0530

    final Pattern

---
+++ Pattern/src/org/star/square.java
@@ -0,0 +1,17 @@
+package org.star;
+
+public class square {
+
+	public static void main(String[] args) {
+		for (int i = 5; i >= 1; i--) {
+			if (i == 1 || i == 5) {
+				System.out.print("****");
+			} else {
+				System.out.print("*  *");
+			}
+			System.out.println("");
+
+		}
+	}
+
+}
